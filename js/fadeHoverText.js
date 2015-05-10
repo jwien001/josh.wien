317e722ed2e3481a8c3ae32196de13528fa40982
Josh Wien <jwien001@gmail.com>
2015-05-10 14:00:35 -0700

    Style edsbo and clean up other styles

--- js/fadeHoverText.js
+++ js/fadeHoverText.js
@@ -1,9 +1,10 @@
 "use strict";
 function fadeText(fadeTo, evt) {
     if (evt.target.id.endsWith("-button")) {
-        children = $("div#button-text").children("p");
+        var children = $("div#button-text").children("p");
         children.stop();
-        if (fadeTo === 1) children.fadeOut(0);
+        if (fadeTo === 1)
+            children.fadeOut(0);
         $("#" + evt.target.id.replace("-button", "-text")).fadeTo(400, fadeTo);
     }
 }
